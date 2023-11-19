af5310bb5cf68982de599a3af098be6d46f8df87
Kuristian <Kuristian@users.noreply.github.com>
2023-11-19 21:58:38 +0000

    mostly working LED controller, timing issues to iron out

--- IO_handler_prototype__arduino_/IO_handler_prototype__arduino_.ino
+++ IO_handler_prototype__arduino_/IO_handler_prototype__arduino_.ino
@@ -29,7 +29,7 @@ int LEDPWM [20];
 
 void SetMultiplexSwitch(int selectedswitch_, bool state);
 bool ReadSwitch (void);
-void SetLEDpin (int state);
+void SetLEDpin (int pin, int state);
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
@@ -37,8 +37,8 @@ void setup() {
   pinMode(12, OUTPUT);
   pinMode(11, OUTPUT);
 
-// initialise LEDs
-//  for (int n = 0; n < 5; n++) SetLEDpin(n, LEDINPUT);
+  // initialise LEDs
+  //  for (int n = 0; n < 5; n++) SetLEDpin(n, LEDINPUT);
 
   pinMode(3, INPUT); // D
   pinMode(4, INPUT); // B
@@ -50,90 +50,42 @@ void setup() {
   Serial.begin(9600);
 
   // Set all LEDs to maximum brightness
-  for (int n = 0; n < 20; n++) LEDPWM = 99;
+  for (int n = 0; n < 20; n++) LEDPWM [n] = 0;
 }
 
 void loop() {
-
-
-
-  
-  //  pinMode(3, INPUT); // D
-  //  pinMode(4, INPUT); // B
-  //  pinMode(5, INPUT); // A
-  //  pinMode(6, INPUT); // E
-  //  pinMode(7, INPUT); // C
-
   cycleshort++;
   if (cycleshort > 4) cycleshort = 0; // cycle between 5 state each loop
   cyclelong++;
   if (cyclelong > 99) cyclelong = 0; // cycle between 100 state each loop
+  if (millis() > 2000)
+  {
+     LEDPWM [1] = 15;
+     LEDPWM [2] = 15;
+     LEDPWM [3] = 15;
+     //LEDPWM [12] = 30;
+  }
 
-  //  if (cycleshort == 0) {
-  //    pinMode(3, OUTPUT); // D
-  //    digitalWrite(3, HIGH);
-  //    pinMode(4, OUTPUT); // C
-  //    pinMode(5, OUTPUT); // A
-  //    pinMode(6, OUTPUT); // E
-  //    pinMode(7, OUTPUT); // C
-  //    digitalWrite(4, LOW);
-  //    digitalWrite(5, LOW);
-  //    digitalWrite(6, LOW);
-  //    digitalWrite(7, LOW);
-  //  }
-  //
-  //  if (cycleshort == 1) {
-  //    pinMode(4, OUTPUT); // D
-  //    digitalWrite(4, HIGH);
-  //    pinMode(3, OUTPUT); // C
-  //    pinMode(5, OUTPUT); // A
-  //    pinMode(6, OUTPUT); // E
-  //    pinMode(7, OUTPUT); // C
-  //    digitalWrite(3, LOW);
-  //    digitalWrite(5, LOW);
-  //    digitalWrite(6, LOW);
-  //    digitalWrite(7, LOW);
-  //  }
-  //
-  //  if (cycleshort == 2) {
-  //    pinMode(5, OUTPUT); // D
-  //    digitalWrite(5, HIGH);
-  //    pinMode(4, OUTPUT); // C
-  //    pinMode(3, OUTPUT); // A
-  //    pinMode(6, OUTPUT); // E
-  //    pinMode(7, OUTPUT); // C
-  //    digitalWrite(4, LOW);
-  //    digitalWrite(3, LOW);
-  //    digitalWrite(6, LOW);
-  //    digitalWrite(7, LOW);
-  //  }
-  //
-  //  if (cycleshort == 3) {
-  //    pinMode(6, OUTPUT); // D
-  //    digitalWrite(6, HIGH);
-  //    pinMode(4, OUTPUT); // C
-  //    pinMode(5, OUTPUT); // A
-  //    pinMode(3, OUTPUT); // E
-  //    pinMode(7, OUTPUT); // C
-  //    digitalWrite(4, LOW);
-  //    digitalWrite(5, LOW);
-  //    digitalWrite(3, LOW);
-  //    digitalWrite(7, LOW);
-  //  }
-  //
-  //  if (cycleshort == 4) {
-  //    pinMode(7, OUTPUT); // D
-  //    digitalWrite(7, HIGH);
-  //    pinMode(3, OUTPUT); // C
-  //    pinMode(5, OUTPUT); // A
-  //    pinMode(6, OUTPUT); // E
-  //    pinMode(4, OUTPUT); // C
-  //    digitalWrite(3, LOW);
-  //    digitalWrite(5, LOW);
-  //    digitalWrite(6, LOW);
-  //    digitalWrite(4, LOW);
-  //  }
+    if (millis() > 4000)
+  {
+     for (int n = 0; n < 20; n++) LEDPWM [n] = 0;
+     LEDPWM [1] = 15;
+     LEDPWM [2] = 99;
+     LEDPWM [3] = 15;
+     LEDPWM [4] = 15;
+     LEDPWM [6] = 15;
+  }
+  // Charlieplex LED controller (as described in the draw.io document)
+  // Set all LEDs as inputs
+  for (int n = 0; n < 5; n++) SetLEDpin(n, LEDINPUT);
+  SetLEDpin (cycleshort, LEDLOW);
+  for (int n = 0; n < 4; n++)
+  {
+    if (LEDPWM[cycleshort * 4 + n] >= cyclelong) SetLEDpin((cycleshort + n + 1) % 5, LEDHIGH);
+    //delay(500);
+  }
 
+  // Multi Switches read (as described in the draw.io document)
   // debug
   //  SetMultiplexSwitch (cycleshort, LOW);
   //  if (ReadSwitch()) inputflag = true;
@@ -142,10 +94,9 @@ void loop() {
 
   //debug
   if (Serial.available() > 0) {
-    // read the incoming byte:
     incomingcommand = Serial.read();
-    //    Serial.print("I received: ");
-    //    Serial.println(incomingcommand, DEC);
+    Serial.print("I received command: ");
+    Serial.println(incomingcommand, DEC);
     serialdebug (incomingcommand);
   }
   if (lastinputstatus != inputstatus)
@@ -154,115 +105,147 @@ void loop() {
     serialdebug ('d');
     serialdebug ('b');
   }
-  //if (cyclelong == 98) serialdebug ('d');
-
   //debug end
+  delayMicroseconds(30);
 }
 
 //debug
 void serialdebug (int incomingcommand_)
 {
-  switch (incomingcommand_)
+  Serial.println("Enter serial debug");
+
+  // set brightness all LEDs
+  if (incomingcommand_ == 'a')
   {
-    case 'd':
-      Serial.print("shortcycle: ");
-      Serial.print(cycleshort);
-      Serial.print("    Longcycle: ");
-      Serial.println(cyclelong);
-      Serial.print("inputstatus: ");
-      Serial.println(inputstatus);
-      Serial.print("inputflag: ");
-      Serial.println(inputflag);
-      Serial.println("");
-      break;
+    Serial.print("Insert brightness from 0 to 99: ");
+    while (Serial.available() == 0);
+    int debugbrightness = Serial.parseInt();
+    for (int n = 0; n < 20; n++) LEDPWM [n] = debugbrightness;
+    Serial.print(" brightness set to: ");
+    Serial.println(debugbrightness);
+  }
 
-    case 'b':
-      Serial.print("SW");
-      Serial.print((inputstatus - 1) / 3 + 1);
-      if (inputstatus % 3 == 1) Serial.println(" Left");
-      if (inputstatus % 3 == 2) Serial.println(" Button");
-      if (inputstatus % 3 == 0) Serial.println(" Right");
-      break;
+  // display switches status
+  if (incomingcommand_ == 'b')
+  {
+    Serial.print("SW");
+    Serial.print((inputstatus - 1) / 3 + 1);
+    if (inputstatus % 3 == 1) Serial.println(" Left");
+    if (inputstatus % 3 == 2) Serial.println(" Button");
+    if (inputstatus % 3 == 0) Serial.println(" Right");
+  }
 
-    // test function
-    case 't':
-      for (int n = 0; n < 5; n++) SetLEDpin(n, LEDINPUT);
-      break;
+  // sets 1 LED's PWM to 99 and disable all others
+  if (incomingcommand_ == 'c')
+  {
+    Serial.print("Enter LED number to light up: ");
+    int debuglightupsingleLED = 0;
+    if (Serial.available() > 0)
+      debuglightupsingleLED = Serial.parseInt();
+    for (int n = 0; n < 20; n++) LEDPWM [n] = 0;
+    LEDPWM [debuglightupsingleLED] = 99;
+    Serial.print(" Light up LED nb: ");
+    Serial.println(debuglightupsingleLED);
+  }
 
-    case'l':
-      Serial.print("enter LED pin: ");
-      bool characterselected = true;
-      int debugLEDPIN = -1;
-      int debugledselected;
-      while (characterselected)
+  // display cycles
+  if (incomingcommand_ == 'd')
+  {
+    Serial.print("shortcycle: ");
+    Serial.print(cycleshort);
+    Serial.print("    Longcycle: ");
+    Serial.println(cyclelong);
+    Serial.print("inputstatus: ");
+    Serial.println(inputstatus);
+    Serial.print("inputflag: ");
+    Serial.println(inputflag);
+    Serial.println("");
+  }
+
+  // test function
+  if (incomingcommand_ == 't')
+  {
+    for (int n = 0; n < 5; n++) SetLEDpin(n, LEDINPUT);
+    Serial.println("set all LEDs to inputs");
+  }
+  
+  // set LEDs
+  if (incomingcommand_ == 'l')
+  {
+    Serial.print("enter LED pin: ");
+    bool characterselected = true;
+    int debugLEDPIN = -1;
+    int debugledselected;
+    while (characterselected)
+    {
+      switch (debugLEDPIN)
       {
-        switch (debugLEDPIN)
-        {
-          case 'a':
-            Serial.print("A selected, enter state: ");
-            characterselected = false;
-            debugledselected = 0;
-            break;
+        case 'a':
+          Serial.print("A selected, enter state: ");
+          characterselected = false;
+          debugledselected = 0;
+          break;
 
-          case 'b':
-            Serial.print("B selected, enter state: ");
-            characterselected = false;
-            debugledselected = 1;
-            break;
+        case 'b':
+          Serial.print("B selected, enter state: ");
+          characterselected = false;
+          debugledselected = 1;
+          break;
 
-          case 'c':
-            Serial.print("C selected, enter state: ");
-            characterselected = false;
-            debugledselected = 2;
-            break;
+        case 'c':
+          Serial.print("C selected, enter state: ");
+          characterselected = false;
+          debugledselected = 2;
+          break;
 
-          case 'd':
-            Serial.print("D selected, enter state: ");
-            characterselected = false;
-            debugledselected = 3;
-            break;
+        case 'd':
+          Serial.print("D selected, enter state: ");
+          characterselected = false;
+          debugledselected = 3;
+          break;
 
-          case 'e':
-            Serial.print("E selected, enter state: ");
-            characterselected = false;
-            debugledselected = 4;
-            break;
+        case 'e':
+          Serial.print("E selected, enter state: ");
+          characterselected = false;
+          debugledselected = 4;
+          break;
 
-          default:
-            if (Serial.available() > 0) debugLEDPIN = Serial.read();
-            break;
-        }
+        default:
+          if (Serial.available() > 0) debugLEDPIN = Serial.read();
+          break;
       }
 
-      bool stateselected = true;
-      debugLEDPIN = -1;
-      while (stateselected)
+    }
+
+    bool stateselected = true;
+    debugLEDPIN = -1;
+    while (stateselected)
+    {
+      switch (debugLEDPIN)
       {
-        switch (debugLEDPIN)
-        {
-          case 'i':
-            Serial.println("Input selected");
-            SetLEDpin (debugledselected, LEDINPUT);
-            stateselected = false;
-            break;
+        case 'i':
+          Serial.println("Input selected");
+          SetLEDpin (debugledselected, LEDINPUT);
+          stateselected = false;
+          break;
 
-          case 'l':
-            Serial.println("Output Low selected");
-            SetLEDpin (debugledselected, LEDLOW);
-            stateselected = false;
-            break;
+        case 'l':
+          Serial.println("Output Low selected");
+          SetLEDpin (debugledselected, LEDLOW);
+          stateselected = false;
+          break;
 
-          case 'h':
-            Serial.println("Output High selected");
-            SetLEDpin (debugledselected, LEDHIGH);
-            stateselected = false;
-            break;
+        case 'h':
+          Serial.println("Output High selected");
+          SetLEDpin (debugledselected, LEDHIGH);
+          stateselected = false;
+          break;
 
-          default:
-            if (Serial.available() > 0) debugLEDPIN = Serial.read();
-            break;
-        }
+        default:
+          if (Serial.available() > 0) debugLEDPIN = Serial.read();
+          break;
       }
+    }
   }
 }
 //debug end

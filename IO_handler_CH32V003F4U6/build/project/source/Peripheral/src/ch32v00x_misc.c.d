984b3c74c2f52d8045ffffd0d0358e32b3c3768f
Kuristian <Kuristian@users.noreply.github.com>
2023-11-25 16:00:36 +0000

    Added base project folder for CH32V003F4U6 Firmware (just blinks on PC0 atm)

---
+++ IO_handler_CH32V003F4U6/build/project/source/Peripheral/src/ch32v00x_misc.c.d
@@ -0,0 +1,46 @@
+project/source/Peripheral/src/ch32v00x_misc.c.o: \
+ ../source/Peripheral/src/ch32v00x_misc.c \
+ ../source/Peripheral/inc/ch32v00x_misc.h \
+ ../source/Peripheral/inc/ch32v00x.h ../source/Core/core_riscv.h \
+ ../source/user_code/system_ch32v00x.h \
+ ../source/user_code/ch32v00x_conf.h \
+ ../source/Peripheral/inc/ch32v00x_adc.h \
+ ../source/Peripheral/inc/ch32v00x_dbgmcu.h \
+ ../source/Peripheral/inc/ch32v00x_dma.h \
+ ../source/Peripheral/inc/ch32v00x_exti.h \
+ ../source/Peripheral/inc/ch32v00x_flash.h \
+ ../source/Peripheral/inc/ch32v00x_gpio.h \
+ ../source/Peripheral/inc/ch32v00x_i2c.h \
+ ../source/user_code/ch32v00x_it.h ../source/Debug/debug.h \
+ ../source/Peripheral/inc/ch32v00x_iwdg.h \
+ ../source/Peripheral/inc/ch32v00x_pwr.h \
+ ../source/Peripheral/inc/ch32v00x_rcc.h \
+ ../source/Peripheral/inc/ch32v00x_spi.h \
+ ../source/Peripheral/inc/ch32v00x_tim.h \
+ ../source/Peripheral/inc/ch32v00x_usart.h \
+ ../source/Peripheral/inc/ch32v00x_wwdg.h \
+ ../source/Peripheral/inc/ch32v00x_opa.h \
+ ../source/Peripheral/inc/ch32v00x.h
+../source/Peripheral/inc/ch32v00x_misc.h:
+../source/Peripheral/inc/ch32v00x.h:
+../source/Core/core_riscv.h:
+../source/user_code/system_ch32v00x.h:
+../source/user_code/ch32v00x_conf.h:
+../source/Peripheral/inc/ch32v00x_adc.h:
+../source/Peripheral/inc/ch32v00x_dbgmcu.h:
+../source/Peripheral/inc/ch32v00x_dma.h:
+../source/Peripheral/inc/ch32v00x_exti.h:
+../source/Peripheral/inc/ch32v00x_flash.h:
+../source/Peripheral/inc/ch32v00x_gpio.h:
+../source/Peripheral/inc/ch32v00x_i2c.h:
+../source/user_code/ch32v00x_it.h:
+../source/Debug/debug.h:
+../source/Peripheral/inc/ch32v00x_iwdg.h:
+../source/Peripheral/inc/ch32v00x_pwr.h:
+../source/Peripheral/inc/ch32v00x_rcc.h:
+../source/Peripheral/inc/ch32v00x_spi.h:
+../source/Peripheral/inc/ch32v00x_tim.h:
+../source/Peripheral/inc/ch32v00x_usart.h:
+../source/Peripheral/inc/ch32v00x_wwdg.h:
+../source/Peripheral/inc/ch32v00x_opa.h:
+../source/Peripheral/inc/ch32v00x.h:
---
+++ IO_handler_CH32V003F4U6/readme.txt
@@ -0,0 +1,19 @@
+microcontroller: ch32v003f4p6
+board: ch32v003f4p6-evt-r0-1v1
+microcontroller manufacturer: wch
+board manufacturer: wch
+icon: icons/folder/closed/blinky.png
+link: https://embeetle.com/#supported-hardware/wch/boards/ch32v003f4p6-evt-r0-1v1
+
+
+info:
+This project was originally a WCH Eclipse sample project which we've slightly
+modified to fit the typical Embeetle project structure.
+
+The original resources can be found on:
+http://www.wch-ic.com/products/CH32V003.html
+
+After unzipping this ZIP-folder, the general source code is copied from
+'CH32V003EVT/EVT/EXAM/SRC/' while the GPIO toggle code is copied from
+'CH32V003EVT/EVT/EXAM/GPIO/GPIO_Toggle/User/'. The linkerscript is found at
+'CH32V003EVT/EVT/EXAM/SRC/Ld/Link.ld'.
